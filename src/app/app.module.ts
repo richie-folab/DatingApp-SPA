cb92de6780f15420b94243846e54aaab279fcd72
‘Richard <richie_folab@yahoo.com>
2019-11-15 20:49:33 +0100

    Added Member Card,Route Resolver and Photo Gallery

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,43 @@
-import { BrowserModule } from '@angular/platform-browser';
+import { BrowserModule, HammerGestureConfig, HAMMER_GESTURE_CONFIG } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {HttpClientModule} from '@angular/common/http';
 import {FormsModule} from '@angular/forms';
-import { BsDropdownModule } from 'ngx-bootstrap';
+import { BsDropdownModule, TabsModule } from 'ngx-bootstrap';
 import { RouterModule } from '@angular/router';
+import { JwtModule } from '@auth0/angular-jwt';
+import { NgxGalleryModule } from 'ngx-gallery';
 
 import { AppComponent } from './app.component';
 import { NavComponent } from './nav/nav.component';
 import { HomeComponent } from './home/home.component';
 import { RegisterComponent } from './register/register.component';
 import { ErrorInterceptorProvider } from './_service/error.interceptor.service';
-import { MemberListComponent } from './member-list/member-list.component';
+import { MemberListComponent } from './members/member-list/member-list.component';
 import { MessagesComponent } from './messages/messages.component';
 import { ListsComponent } from './lists/lists.component';
 import { appRoutes } from './routes';
+import { AlertifyService } from './_service/alertify.service';
+import { AuthGuard } from './_guards/auth.guard';
+import { MemberCardComponent } from './members/member-card/member-card.component';
+import { MemberDetailComponent } from './members/member-detail/member-detail.component';
+import { MemberListResolver } from './_resolver/member-list.resolver';
+import { MemberDetailResolver } from './_resolver/member-detail.resolver';
+import { UserService } from './_service/user.service';
 
+export function tokenGetter(){
+   return localStorage.getItem('token');
+}
 
+export class CustomHammerConfig extends HammerGestureConfig {
+   overrides = {
+      pinch: {
+          enable: false
+      },
+      rotate: {
+          enable: false
+      }
+  };
+}
 
 @NgModule({
    declarations: [
@@ -25,17 +47,34 @@ import { appRoutes } from './routes';
       RegisterComponent,
       MemberListComponent,
       MessagesComponent,
-      ListsComponent
+      ListsComponent,
+      MemberCardComponent,
+      MemberDetailComponent
    ],
    imports: [
       BrowserModule,
       HttpClientModule,
       FormsModule,
       BsDropdownModule.forRoot(),
-      RouterModule.forRoot(appRoutes)
+      RouterModule.forRoot(appRoutes),
+      TabsModule.forRoot(),
+      NgxGalleryModule,
+      JwtModule.forRoot({
+         config: {
+            tokenGetter,
+            whitelistedDomains: ['localhost:5000'],
+            blacklistedRoutes: ['localhost:5000/api/auth']
+         }
+      })
    ],
    providers: [
-      ErrorInterceptorProvider
+      ErrorInterceptorProvider,
+      AlertifyService,
+      AuthGuard,
+      UserService,
+      MemberDetailResolver,
+      MemberListResolver,
+      {provide: HAMMER_GESTURE_CONFIG, useClass:CustomHammerConfig}
    ],
    bootstrap: [
       AppComponent
